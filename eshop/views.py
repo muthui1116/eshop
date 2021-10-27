b801e42de92895f5945a192c27825b1667be4b0e
Muthui Kyalo <muthuikyalozis1@gmail.com>
2021-10-27 19:06:14 +0300

    initial complete

---
+++ eshop/views.py
@@ -0,0 +1,12 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+from store.models import Product
+
+
+def index(request):
+    # return HttpResponse('html page'),
+    products = Product.objects.all().filter(is_available=True)
+    context = {
+        'products': products
+    }
+    return render(request, 'index.html', context)
